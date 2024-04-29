5b3ff46440a0b4d685d897af32df85680588be41
Hafizh Fauzan <hafizhfauzan3030@gmail.com>
2024-04-29 17:34:26 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="input-container">
+      <input type="text" v-model="inputText" @input="checkInput" :class="{ 'input-error': inputError }">
+      <span v-if="inputError" class="input-error">{{ errorMessage }}</span>
+    </div>
+    <p v-if="inputText.length > 0">Length of Input Text: {{ inputText.length }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const inputText = ref('');
+const inputError = ref(false);
+const errorMessage = ref('');
+
+function checkInput() {
+  if (inputText.value.length > 10) {
+    inputError.value = true;
+    errorMessage.value = 'Input text is too long!';
+  } else {
+    inputError.value = false;
+    errorMessage.value = '';
+  }
+}
+</script>
+
+<style>
+.input-container {
+  margin-bottom: 10px;
+}
+
+.input-error {
+  color: red;
+}
+
+.input-error + input {
+  border-color: red;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="window" @click="stackBook" v-if="!canSeeOutside">
+      <button class="book" v-for="(book, index) in books" :key="index" @click="stackBook"></button>
+      <div class="person" :style="{ bottom: books.length * 20 + 'px' }"></div>
+    </div>
+    <p v-if="canSeeOutside">Moral Pendidikan: Belajar adalah kunci keberhasilan.</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const books = ref([])
+const canSeeOutside = ref(false)
+
+function stackBook() {
+  if (!canSeeOutside.value) {
+    books.value.push({})
+    if (books.value.length >= 3) {
+      canSeeOutside.value = true
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  position: relative;
+  width: 400px;
+  height: 600px;
+  background-color: #f0f0f0;
+  overflow: hidden;
+}
+
+.window {
+  position: absolute;
+  width: 300px;
+  height: 400px;
+  background-color: #fff;
+  border: 2px solid #000;
+  top: 100px;
+  left: 50px;
+}
+
+.book {
+  position: absolute;
+  width: 60px;
+  height: 80px;
+  background-color: #ffcc00;
+  border: 2px solid #000;
+  cursor: pointer;
+}
+
+.person {
+  position: absolute;
+  width: 40px;
+  height: 100px;
+  background-color: #ff0000;
+  bottom: 0;
+}
+</style>
